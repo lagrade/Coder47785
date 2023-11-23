23c0da49ff02ec66d85f6d2ec4a495ddc534d989
laura desimone <desimone.lau@gmail.com>
2023-11-22 22:36:16 -0300

    crear busqueda

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppCoder.views import crear_curso, show_html, mostrar_cursos
+from AppCoder.views import crear_curso, show_html, mostrar_cursos, crear_curso_form, Busqueda_camada
 
 urlpatterns = [
 
     path("agregar_curso/", crear_curso),
+    path("curso/", crear_curso_form),
+    path("buscar/", Busqueda_camada ),
     path("show/", show_html),
     path("cursos/", mostrar_cursos),
 
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from AppCoder.models import Curso
+from AppCoder.form import CursoForm, BusquedaCursoForm
 from django.template import loader
 from django.http import HttpResponse
 
@@ -9,8 +10,25 @@ def crear_curso(request):
      curso.save()
      return redirect("/app/cursos/")
 
+def crear_curso_form (request):
+     if request.method == "POST":
+          curso_formulario =CursoForm(request.POST)
+          if curso_formulario.is_valid():
+               informacion =curso_formulario.cleaned_data
 
+          curso_crear = Curso(nombre=informacion["nombre"], camada=informacion ["camada"])
+          curso_crear.save()
 
+          return redirect("/app/curso/")
+     curso_formulario = Curso.objects.all()
+     contexto = {
+          "form": curso_formulario
+     }
+     return render(request, "AppCoder/crearcurso.html", contexto)
+
+def Busqueda_camada(request):
+     nombre= request.GET["nombre"]
+     curso= Curso.objects.filter(nombre_icontains= nombre)
 def show_html(request):
      curso = Curso.objects.first()
      contexto ={ "curso" : curso, "nombre": "Laura"}
@@ -20,6 +38,7 @@ def mostrar_cursos(request):
      cursos = Curso.objects.all()
      contexto = {
           "cursos": cursos,
-          "nombre": "laura"
+          "nombre": "laura",
+          "form": BusquedaCursoForm(),
      }
      return render(request, "AppCoder/cursos.html", contexto)
